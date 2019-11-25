a4ed5e708395471e654bd2ca632a7555a8b4e5c0
pengxu <pengxu19890115@gmail.com>
2019-11-25 12:04:35 -0800

    AppColorForm, Color, ColorLisr js

---
+++ src/AddColorForm.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const AddColorForm = ({ onNewColor = f => f }) => {
+  let title, color;
+
+  const submit = e => {
+    e.preventDefault();
+    onNewColor(title.value, color.value);
+    title.value = '';
+    color.value = '#000000';
+    title.focus();
+  };
+
+  return (
+    <form className="add-color" onSubmit={submit}>
+      <input
+        ref={input => (title = input)}
+        type="text"
+        placeholder="color title..."
+        required
+      />
+      <input ref={input => (color = input)} type="color" required />
+      <button>ADD</button>
+    </form>
+  );
+};
+
+AddColorForm.propTypes = {
+  onNewColor: PropTypes.func
+};
+
+export default AddColorForm;
